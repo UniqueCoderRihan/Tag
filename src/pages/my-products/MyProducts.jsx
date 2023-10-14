9c8a60c705031e781bd924259370c802279e25aa
uniquecoderrihan <rihanthedev@gmail.com>
2023-10-14 09:30:52 +0600

    add new Product done

--- src/pages/my-products/MyProducts.jsx
+++ src/pages/my-products/MyProducts.jsx
@@ -1,11 +1,23 @@
 import { useEffect, useState } from "react";
 import { useForm } from 'react-hook-form';
 import Swal from "sweetalert2";
+import * as Yup from 'yup';
+import { yupResolver } from '@hookform/resolvers/yup';
+const validationSchema = Yup.object().shape({
+    productName: Yup.string().required('Product Name is required'),
+    prouctImages: Yup.string().url('Invalid image link').required('Image link is required'),
+    price: Yup.number().typeError('Price must be a number').required('Price is required').positive('Price must be positive'),
+    category: Yup.string().required('Product Category is required'),
+    avaibleQuantity: Yup.number().typeError('Available Quantity must be a number').required('Available Quantity is required').positive('Available Quantity must be positive'),
+    description: Yup.string().required('Description is required'),
+});
 
 const MyProducts = () => {
     const [products, setProducts] = useState([]);
     const [searchText, setText] = useState('');
-    const { register, handleSubmit, setValue } = useForm({});
+    const { register, handleSubmit, errors,reset } = useForm({
+        resolver: yupResolver(validationSchema),
+    });
     const [isModalOpen, setIsModalOpen] = useState(false);
     const openModal = () => {
         setIsModalOpen(true);
@@ -21,33 +33,52 @@ const MyProducts = () => {
             .then(data => setProducts(data))
     }
     const onSubmit = async (data) => {
-        Swal.fire({
-            position: 'top-end',
-            icon: 'success',
-            title: 'Product Added successfully',
-            showConfirmButton: false,
-            timer: 1500,
-        });
-        // const event = {
-        //     title: data.title,
-        //     banner: data.banner,
-        //     relase: data.relase,
-        // };
-        // console.log(event);
-        // try {
-        //     const EventAdded = await addNewEvent(event);
-        //     Swal.fire({
-        //         position: 'top-end',
-        //         icon: 'success',
-        //         title: 'Event Added successfully',
-        //         showConfirmButton: false,
-        //         timer: 1500,
-        //     });
-        //     closeModal();
-        // } catch (err) {
-        //     console.log(err);
-        // }
-    };
+        console.log(data);
+        const product = {
+            productName: data.productName,
+            prouctImages: data.prouctImages,
+            price: data.price,
+            author: 'Tag-Think static',
+            description: data.description,
+            avaibleQuantity: data.avaibleQuantity,
+            category: data.category,
+            sellerEmail: 'raihanvai@gmail.com'
+        }
+        // use realtime user info TODO::::
+        // console.log(product);
+        fetch('http://localhost:3000/addProduct', {
+            method: 'POST',
+            headers: {
+                'Content-type': 'application/json'
+            },
+            body: JSON.stringify(product)
+        })
+            .then(res => res.json())
+            .then(data => {
+                console.log(data);
+                if (data.acknowledged) {
+                    Swal.fire({
+                        title: 'Success!',
+                        text: 'ThankYou!  Added!!',
+                        icon: 'success',
+                        confirmButtonText: 'Done'
+                    })
+                    
+                    closeModal(true)
+                    reset()
+                }
+                else {
+                    Swal.fire({
+                        title: 'Error!',
+                        text: 'Please Try Again Letter',
+                        icon: 'error',
+                        confirmButtonText: 'Cool'
+                    })
+                }
+            })
+    }
+
+
 
     useEffect(() => {
         fetch('http://localhost:3000/products')
@@ -97,6 +128,9 @@ const MyProducts = () => {
                                     required
                                     {...register('productName')}
                                 />
+                                {errors?.productName && (
+                                    <p className="text-red-500">{errors.productName.message}</p>
+                                )}
                             </div>
                             <div className="w-[100%] mx-auto mt-1">
                                 <label
@@ -111,6 +145,9 @@ const MyProducts = () => {
                                     required
                                     {...register('prouctImages')}
                                 />
+                                {errors?.prouctImages && (
+                                    <p className="text-red-500">{errors.prouctImages.message}</p>
+                                )}
                             </div>
 
                             <div className="w-[100%] mx-auto mt-1">
@@ -126,6 +163,9 @@ const MyProducts = () => {
                                     required
                                     {...register('price')}
                                 />
+                                {errors?.price && (
+                                    <p className="text-red-500">{errors.price.message}</p>
+                                )}
                             </div>
                             <div className="w-[100%] mx-auto mt-1">
                                 <label
@@ -134,13 +174,21 @@ const MyProducts = () => {
                                 >
                                     Category
                                 </label>
-                                <input
+                                <select
                                     className="mt-1 p-2 w-full text-black border rounded focus:outline-none focus:border-blue-300"
-                                    type="number"
                                     required
                                     {...register('category')}
-                                />
+                                >
+                                    <option value="tshirt">T-Shirt</option>
+                                    <option value="laptop">Laptop</option>
+                                    <option value="tv">TV</option>
+                                    <option value="fish">Fish</option>
+                                </select>
+                                {errors?.category && (
+                                    <p className="text-red-500">{errors.category.message}</p>
+                                )}
                             </div>
+
                             <div className="w-[100%] mx-auto my-1">
                                 <label
                                     htmlFor="avaibleQuantity"
@@ -153,7 +201,10 @@ const MyProducts = () => {
                                     type="number"
                                     required
                                     {...register('avaibleQuantity')}
-                                />
+
+                                />{errors?.avaibleQuantity && (
+                                    <p className="text-red-500">{errors.avaibleQuantity.message}</p>
+                                )}
                             </div>
                             <div className="w-[100%] mx-auto my-1">
                                 <label
@@ -170,6 +221,9 @@ const MyProducts = () => {
                                 /> */}
                                 <textarea required
                                     {...register('description')} id="description" name="description" rows="4" cols="50"></textarea>
+                                {errors?.description && (
+                                    <p className="text-red-500">{errors.description.message}</p>
+                                )}
                             </div>
 
                             <button
